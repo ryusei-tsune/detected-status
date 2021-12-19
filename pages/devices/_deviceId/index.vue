7d0d430cee1bb0d760a70dca4f9286cfb3c44f74
Ryusei <rt1226.tennis@gmail.com>
2021-12-19 11:10:47 +0900

    update graph options

---
+++ pages/devices/_deviceId/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-container>
+    <!-- {{ $route.params.deviceId }}の詳細 -->
+    <v-card-title v-if="resName"> {{ resName[0].name }}({{ resName[0].id }}) </v-card-title>
+    <v-row justify="center">
+      <v-col cols="12" style="text-align: center" v-if="temperature">
+        temperature
+        <Chart :data="temperature" :chartOptions="temperatureOptions" />
+      </v-col>
+      <v-col cols="12" style="text-align: center" v-if="humidity">
+        humidity
+        <Chart :data="humidity" :chartOptions="humidityOptions" />
+      </v-col>
+      <v-col cols="12" style="text-align: center" v-if="brightness_level">
+        brightness_level
+        <Chart
+          :data="brightness_level"
+          :chartOptions="brightness_levelOptions"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import Chart from "@/components/Chart.vue";
+//import Chart from "../../../components/Chart.vue";
+export default {
+  head() {
+    return {
+      title: "",
+    };
+  },
+  layout: "default",
+  components: {
+    Chart,
+  },
+  middleware: [],
+  data() {
+    return {
+      temperatureOptions: { chart: { title: "temperature" }, colors: ['#1b9e77'],  backgroundColor: '#f1f8e9' },
+      humidityOptions: { chart: { title: "humidity" }, colors: ['#d95f02'] },
+      brightness_levelOptions: { chart: { title: "brightness_level" }, colors: ['#7570b3'] },
+      resData: null,
+      resName: null,
+      temperature: null,
+      humidity: null,
+      brightness_level: null,
+    };
+  },
+  watch: {},
+  computed: {},
+  created() {},
+  beforeMount() {},
+  mounted() {
+    this.getData();
+  },
+  beforeDestroy() {},
+  methods: {
+    async getData() {
+      try {
+        const { data } = await this.$axios.get(
+          "/api/devices/" + this.$route.params.deviceId + "/measured-data"
+        );
+        this.$set(this, "resData", data.body);
+        this.$set(this, "resName", data.name);
+        console.log(this.resName)
+        this.temperature = [];
+        this.humidity = [];
+        this.brightness_level = [];
+        this.temperature.push(["time", "temperature"]);
+        this.humidity.push(["time", "humidity"]);
+        this.brightness_level.push(["time", "brightness_level"]);
+        this.resData.forEach((row) => {
+          this.temperature.push([row.created_at, Number(row.temperature)]);
+          this.humidity.push([row.created_at, Number(row.humidity)]);
+          this.brightness_level.push([
+            row.created_at,
+            Number(row.brightness_level),
+          ]);
+        });
+      } catch (err) {
+        console.log("getData()", err);
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
