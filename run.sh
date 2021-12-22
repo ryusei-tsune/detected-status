e8e4472c49578639e206340452d2a4121cb1b610
ryusei-tsune <rt1226.tennis@gmail.com>
2021-12-23 01:15:54 +0900

    Create production version docker-compose

--- run.sh
+++ run.sh
@@ -8,12 +8,21 @@ elif test "$1" = "down"; then
   docker-compose down
 elif test "$1" = "logs"; then
   docker-compose logs -f
+
 elif test "$1" = "up-prod"; then
-  U=$(id -u) G=$(id -g) docker-compose -f docker-compose.gce.yml up -d
+  U=$(id -u) G=$(id -g) docker-compose -f docker-compose.gce-prod.yml up -d
 elif test "$1" = "down-prod"; then
-  docker-compose -f docker-compose.gce.yml down
+  docker-compose -f docker-compose.gce-prod.yml down
 elif test "$1" = "logs-prod"; then
-  docker-compose -f docker-compose.gce.yml logs -f
+  docker-compose -f docker-compose.gce-prod.yml logs -f
+
+elif test "$1" = "up-local"; then
+  U=$(id -u) G=$(id -g) docker-compose -f docker-compose.gce-local.yml up -d
+elif test "$1" = "down-local"; then
+  docker-compose -f docker-compose.gce-local.yml down
+elif test "$1" = "logs-local"; then
+  docker-compose -f docker-compose.gce-local.yml logs -f
+
 else
   echo "Usage"
   echo "bash run.sh up"
@@ -23,4 +32,7 @@ else
   echo "bash run.sh up-prod"
   echo "bash run.sh down-prod"
   echo "bash run.sh logs-prod"
+  echo "bash run.sh up-local"
+  echo "bash run.sh down-local"
+  echo "bash run.sh logs-local"
 fi
